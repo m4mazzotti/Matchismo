ae0ca5dd661a2c0203255a120e94d5006caa067c
Marcelo Mazzotti Paes de Almeida <m4mazzotti@gmail.com>
2013-04-22 16:24:05 -0400

    Add: All code from Lecture 5
    This also includes the sort functionality: Date, Score and Duration.

---
+++ Matchismo/GameResult.h
@@ -0,0 +1,13 @@
+//
+//  GameResult.h
+//  Matchismo
+//
+//  Created by Marcelo Mazzotti on 21/4/13.
+//  Copyright (c) 2013 Marcelo Mazzotti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameResult : NSObject
+
+@end
---
+++ Matchismo/GameResult.m
@@ -0,0 +1,13 @@
+//
+//  GameResult.m
+//  Matchismo
+//
+//  Created by Marcelo Mazzotti on 21/4/13.
+//  Copyright (c) 2013 Marcelo Mazzotti. All rights reserved.
+//
+
+#import "GameResult.h"
+
+@implementation GameResult
+
+@end
